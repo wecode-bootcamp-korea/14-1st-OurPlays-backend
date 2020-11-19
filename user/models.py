90b91e489ab7da83c03b5fb76955ee2847ce083c
영구킴 <a097@097.local>
2020-11-19 16:03:21 +0900

    [ADD]회원가입 및 로그인

--- user/models.py
+++ user/models.py
@@ -1,22 +1,21 @@
 from django.db    import models
 
 class User(models.Model):
-    name                 = models.CharField(max_length=100)
-    shoot_count          = models.IntegerField(default=0)
-    email                = models.EmailField(max_length=50)
-    password             = models.CharField(max_length=50)
-    introduce            = models.CharField(max_length=300)
-    agree_receive_email  = models.BooleanField(default=False)
-    agree_recommand_area = models.BooleanField(default=False)
-    thumbnail_url        = models.URLField(max_length=200)   
-    created_at           = models.DateTimeField(auto_now_add=True)
+    name          = models.CharField(max_length=100)
+    shoot_count   = models.IntegerField(default=0)
+    email         = models.EmailField(max_length=50)
+    password      = models.CharField(max_length=300)
+    introduce     = models.CharField(max_length=300, null=True, default="안녕하세요!")
+    thumbnail_url = models.URLField(max_length=1000, null=True)   
+    created_at    = models.DateTimeField(auto_now_add=True)
     
     class Meta:
         db_table = 'users'
         
 class Placemark(models.Model):
-    user  = models.ForeignKey(User,on_delete=models.CASCADE)
-    place = models.ForeignKey('place.Place',on_delete=models.CASCADE)
+    user       = models.ForeignKey(User, on_delete=models.CASCADE)
+    place      = models.ForeignKey('place.Place', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         db_table = 'place_marks'
@@ -29,7 +28,7 @@ class UserTag(models.Model):
         db_table = 'user_tags'
 
 class SignupMotive(models.Model):
-    user   = models.ForeignKey(User,on_delete=models.CASCADE)
+    user   = models.ForeignKey(User, on_delete=models.CASCADE)
     motive = models.CharField(max_length=100)
     
     class Meta:
